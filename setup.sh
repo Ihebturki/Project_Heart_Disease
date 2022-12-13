35adb949c327a36b82e94e290158454f7d369d86
Ihebturki <115578927+Ihebturki@users.noreply.github.com>
2022-12-13 18:20:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,10 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+[theme]
+primaryColor="#2214c7"
+backgroundColor="#ffffff"
+secondaryBackgroundColor="#e8eef9"
+textColor="#000000"
+font="sans serif"
+ > ~/.streamlit/config.toml
