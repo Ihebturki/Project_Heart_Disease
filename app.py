9847cd920c35866c5ea34c20f0ce9efc3d1e6e5e
Ihebturki <115578927+Ihebturki@users.noreply.github.com>
2022-12-14 20:31:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import plotly.express as px
 from streamlit_option_menu import option_menu
 
 
-
+page_bg_img = f"""
 <style>
 {{
 primaryColor = "#d33682"
@@ -24,6 +24,7 @@ font-family = "Cooper Black"
 font-size:50px
 }}
 </style>
+"""
 
 
             
