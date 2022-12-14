806bad0e676ad714722ec2d0b6c97761ee72800c
Ihebturki <115578927+Ihebturki@users.noreply.github.com>
2022-12-14 20:20:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ background-attachment: local;
 primaryColor = "#d33682"
 backgroundColor = "#002b36"
 secondaryBackgroundColor = "#586e75"
-textColor = "#fafafa"
-font = "sans serif"
+textColor = "red"
+font = "monospace"
 }}
 </style>
 """
